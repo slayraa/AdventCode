0b4334c2ce179d0789d612280bd6110f845ccff7
Slayraa <ipereiramatos@gmail.com>
2023-12-01 16:39:22 +0000

    Final solution for day 1

--- 2023/01/main_p2.py
+++ 2023/01/main_p2.py
@@ -6,8 +6,10 @@ puzzle_input = "2023/inputs/01.txt"
 f = open(puzzle_input,"r")
 lines = f.readlines()
 
-numbers_list = r'(\d|one|two|three|four|five|six|seven|eight|nine)'
+numbers_list = r'(\d|zero|one|two|three|four|five|six|seven|eight|nine)'
+numbers_list_reversed = r'(\d|orez|eno|owt|eerht|ruof|evif|xis|neves|thgie|enin)'
 numbers_map = {
+    'zero': '0',
     'one': '1',
     'two': '2',
     'three': '3',
@@ -21,11 +23,15 @@ numbers_map = {
 
 to_sum = []
 for line in lines:
-    numbers = re.findall(numbers_list, line.replace("\n",""))
-    numbers = [numbers_map[num] if num in numbers_map else num for num in numbers]
-    to_sum.append(numbers[0] + numbers[-1])
+    first_number = re.findall(numbers_list, line)[0]
+    reversed_line = line[::-1]
+    last_number = re.findall(numbers_list_reversed, reversed_line)[0][::-1]
+    numbers = [first_number, last_number]
+    numbers_mapped = [numbers_map.get(num) if num in numbers_map.keys() else num for num in numbers]
+    #print(f"line: {line} numbers: {numbers} to_sum: {numbers_mapped[0] + numbers_mapped[-1]}")
+    to_sum.append(numbers_mapped[0] + numbers_mapped[1])
 
 sum_f = sum([int(num) for num in to_sum])
 
 print(f"What is the sum of all of the calibration values? Answer: {sum_f}")
-#55725
+#55701
