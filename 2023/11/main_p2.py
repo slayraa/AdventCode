fef3fb12a95c4c7604b391d9f5293d2bdc6bfa08
Slayraa <ipereiramatos@gmail.com>
2023-12-11 14:35:14 +0000

    Add solutions to day 11 part1 and part2

---
+++ 2023/11/main_p2.py
@@ -0,0 +1,50 @@
+import numpy as np
+
+puzzle_input = "2023/inputs/11.txt"
+#puzzle_input = "2023/inputs/11_sample.txt"
+
+# Read the text file
+with open(puzzle_input, "r") as file:
+    lines = file.readlines()
+
+# Convert each character to a NumPy matrix
+m_puzzle = np.array([list(line.replace("\n","").strip()) for line in lines])
+
+# Find rows and columns where all symbols are "."
+dotted_rows = np.where(np.all(m_puzzle == ".", axis=1))[0]
+dotted_cols = np.where(np.all(m_puzzle == ".", axis=0))[0]
+
+# Replace "#" in m_puzzle with range of ints from 1 to number of #s
+galax_coordinates = np.argwhere(m_puzzle == "#")
+m_puzzle[m_puzzle == "#"] = np.arange(1, np.sum(m_puzzle == "#") + 1)
+
+distances = []
+expansion = 1000000
+# Find the Manhattan distance between all pairs of #s
+for i in range(len(galax_coordinates)-1):
+    for j in range(i+1, len(galax_coordinates)):
+
+        [row1, col1] = galax_coordinates[i]
+        [row2, col2] = galax_coordinates[j]
+
+        # Only calculate distance between different galaxies
+        if [row1, col1] != [row2, col2]:
+            
+            # Find the number of empty rows and columns that the galaxy crosses
+            rows_crossed = [r for r in range(min(row1, row2), max(row1, row2)) if r in dotted_rows] # increases row number
+            cols_crossed = [c for c in range(min(col1, col2), max(col1, col2)) if c in dotted_cols] # increases column number
+
+            # Find the start and end coordinates of the galaxy
+            galax_start = np.array([row1, col1])
+            galax_end = np.array([row2, col2])
+
+            # Calculate the Manhattan distance between the two galaxies
+            distance = np.linalg.norm(galax_end - galax_start, 1) + len(rows_crossed)*(expansion-1) + len(cols_crossed)*(expansion-1)
+            
+            distances.append(distance)
+            #print(f"The distance between galaxy {m_puzzle[row1,col1]} and {m_puzzle[row2,col2]} is {distance}")
+
+print(f"What is the sum of these lengths? Answer: {np.sum(distances)}")
+# Sample for 10x is 1030
+# Sample for 100x is 8410
+# 504715068438
